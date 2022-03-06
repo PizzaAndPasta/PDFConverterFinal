33119344479b91d882a2897772b4b64836caa1e4
PizzaAndPasta <kevin.koster1997@protonmail.com>
2022-03-06 20:48:08 +0100

    Initial commit

---
+++ PDFConverter.Demo/PDFConverter.Demo/PDFConverter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aspose.Pdf;
+using Aspose.Words;
+
+namespace PDFConverter.Demo
+{
+
+    public class PDFConverter {
+        private string source;
+        private string destination;
+        private Boolean watermark;
+
+        public PDFConverter(string quelle, string ziel, Boolean stempel)
+        {
+            source = quelle;
+            destination = ziel;
+            watermark = stempel;
+
+            /*
+             Zuerst wird geprüft, ob es sich um eine docx oder jpg Datei handelt,
+             da hierfür entweder Aspose.Words oder Aspose.Pdf benutzt werden muss.
+             Das konvertieren geschieht dann in einer der beiden Funktionen.
+            */
+            if (source.Contains(".jpg"))
+            {
+                ConvertFromJpg();
+            }
+            else
+            {
+                ConvertFromDocx();
+            }
+        }
+            
+        
+       
+    
+
+            private void ConvertFromDocx()
+            {
+                //Neues Dokument erstellen
+                Aspose.Words.Document doc = new Aspose.Words.Document(source);
+
+                //Bei Aspose.Words kann man mit nur einem Befehl automatisch ein ausgerichtetes Wasserzeichen auf das Dokument schreiben.
+                if (watermark == true)
+                {
+                    doc.Watermark.SetText("Genehmigt!");
+                }
+                doc.Save(destination + ".pdf");
+            }
+
+            private void ConvertFromJpg()
+            {
+                //Neues PDF Dokument erstellen
+                Aspose.Pdf.Document doc = new Aspose.Pdf.Document();
+
+
+                //Leere Seite im leeren Dokument hinzufügen
+                Page page = doc.Pages.Add();
+                Aspose.Pdf.Image image = new Aspose.Pdf.Image();
+                image.File = (source);
+
+                //Image zur Page hinzufügen
+                page.Paragraphs.Add(image);
+
+                // Prüfen, ob dem Dokument ein Wasserzeichen hinzugefügt werden soll.
+
+                if (watermark == true)
+                {
+                    // Es wird ein WatermarkAnnotation namens water erstellt und mittig auf dem Dokument plaziert.
+                    
+                    Aspose.Pdf.Annotations.WatermarkAnnotation water = new Aspose.Pdf.Annotations.WatermarkAnnotation(page, new Aspose.Pdf.Rectangle(100, 500, 1000, 1000));
+                    
+                    page.Annotations.Add(water);
+
+                    //Es wird ein TextElement erstellt, schwarze Schrift, Schriftart: Arial, 70 Fontsize.
+                    Aspose.Pdf.Text.TextState text = new Aspose.Pdf.Text.TextState();
+
+                    text.ForegroundColor = Aspose.Pdf.Color.Black;
+                    text.Font = Aspose.Pdf.Text.FontRepository.FindFont("Arial");
+                    text.FontSize = 70;
+
+                    // Das Wasserzeichen bekommt 0.8 Opacity und der Text wird mit dem gewünschten Inhalt hinzugefügt.
+                    water.Opacity = 0.8;
+                    water.SetTextAndState(new string[] { "Genehmigt!" }, text);
+
+                }
+
+                //PDF mit dem beim Aufruf gewünschten Namen abspeichern.
+                doc.Save(destination + ".pdf");
+        }
+
+        
+    } 
+
+ }         
---
+++ PDFConverter.Demo/PDFConverter.Demo/Program.cs
@@ -0,0 +1,60 @@
+﻿using Aspose.Pdf;
+using System;
+
+namespace PDFConverter.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Beim Aufruf der Klasse muss der Pfad entsprechend angepasst werden.
+             * Im Ordner "Files" ist ein Bild (A45), ein Docx Dokument (TestDOC).
+             * Die in PDF konvertieren Dokumente werden ebenfalls unter Files abgespeichert und heißen dann hier "DOCxTest" und "JPGTest", jeweils mit und ohne Wasserzeichen.
+             * 
+             * Ich hatte das Programm, nachdem es die gewünschten Ergebnisse geliefert hat, versucht zu ordnen, indem das Konvertieren in die beiden
+             * Methoden "ConvertFromDocx()" und "ConvertFromJpg()" geschieht, welche dann im Konstruktor aufgerufen werden.
+             * 
+             * 
+             */
+
+            
+            PDFConverter PdfBildWatermark = new PDFConverter(@"...\PDFConverterFinal\Files\A45.jpg", @"...\PDFConverterFinal\Files\JPGTestWatermark", true);
+
+            PDFConverter PdfDocWatermark = new PDFConverter(@"...\PDFConverterFinal\Files\TestDOC.docx", @"...\PDFConverterFinal\Files\DOCxTestWatermark", true);
+
+            PDFConverter PdfBildNoWatermark = new PDFConverter(@"...\PDFConverterFinal\Files\A45.jpg", @"...\PDFConverterFinal\Files\JPGTestNoWatermark", false);
+
+            PDFConverter PdfDocNoWatermark = new PDFConverter(@"...\PDFConverterFinal\Files\TestDOC.docx", @"...\PDFConverterFinal\Files\DOCxTestNoWatermark", false);
+
+
+
+
+
+            /*
+             * 
+             * Problemstellung:
+             * Unser Kunde hat ein Webportal wo sämtliche seiner Kunden
+             * Dokumente hochladen. Die Dokumente werden immer in eine PDF konvertiert
+             * und wenn die Dokumente genehmigt werden auch noch mit einem Stempel versehen
+             * 
+             * 
+             * Um Die Aufgabe klein zuhalten fokussieren wir uns nur auf
+             * folgende Formate .docx und .jpg. 
+             * 
+             * Wir benötigen also eine Klasse die als Parameter folgendes empfängt
+             *      Quellpfad ( .docx oder .jpg )
+             *      Zielpfad ( -> wohin soll die Datei geschrieben werden ? )
+             *      Ein Parameter ob entsprechende Datei ein Stempel/Wasserzeichen auf das
+             *      Dokument drucken soll. 
+             *      
+             *      
+             *      Die Bibliothek die wir in diesem Beispiel nutzen ist
+             *      https://blog.aspose.com/2020/07/26/images-to-pdf-csharp/
+             *
+             */
+
+
+        }
+    }
+}
